7168c26d8f91cbe74cd0e52daef49af50392e94c
Brian Woodward <brian.woodward@gmail.com>
2015-10-15 11:18:29 -0400

    formatting

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,29 @@ import {readdirSync as read} from 'fs';
 import {join} from 'path';
 import webpack from 'webpack';
 
-const nodeModules = read(join(process.cwd(), 'node_modules')).reduce((o, mod) => {
-  if (mod !== '.bin') {
-    o[mod] = `commonjs ${mod}`;
-  }
-  return o;
-}, {});
+const nodeModules = read(join(process.cwd(), 'node_modules'))
+  .reduce((o, mod) => {
+    if (mod !== '.bin') {
+      o[mod] = `commonjs ${mod}`;
+    }
+    return o;
+  }, {});
 
-const loaders = [
-  {
-    test: /\.js?$/,
-    exclude: /node_modules/,
-    loader: 'babel?optional[]=runtime&stage=0'
-  },
-  {
-    test: /\.json?$/,
-    loader: 'json'
-  }
-];
+const loaders = [{
+  test: /\.js?$/,
+  exclude: /node_modules/,
+  loader: 'babel?optional[]=runtime&stage=0'
+}, {
+  test: /\.json?$/,
+  loader: 'json'
+}];
 
-const plugins = [
-  new webpack.BannerPlugin(
-    'try{require("source-map-support").install();}\ncatch(err) {}',
-    { raw: true, entryOnly: false }
-  )
-];
+const bannerPlugin = new webpack.BannerPlugin('try{require("source-map-support").install();}\ncatch(err) {}', {
+  raw: true,
+  entryOnly: false
+});
 
+const plugins = [bannerPlugin];
 const entry = {
   index: './index.js'
 };
