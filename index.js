be9898c477fd7566d28b9e065566d89ed68f4216
Brian Woodward <brian.woodward@gmail.com>
2015-11-25 18:06:58 -0500

    refactor main application to use cyborg-store and better provider handling

--- index.js
+++ index.js
@@ -7,7 +7,10 @@
 
 var path = require('path');
 var base = require('base-methods');
+
+var CyborgStore = require('./lib/cyborg-store');
 var utils = require('./lib/utils');
+var providers = require('./lib/providers');
 
 var Base = base.namespace('cache');
 
@@ -15,67 +18,120 @@ function Cyborg(options) {
   if (!(this instanceof Cyborg)) {
     return new Cyborg(options);
   }
+
   Base.call(this);
   this.options = options || {};
-  this.define('providers', {});
   this.use(utils.use);
   this.use(utils.option());
+  this.use(providers());
+  this.define('manifests', new CyborgStore());
   this.use(utils.store('global', {cwd: '~/.cyborg/.datastore'}));
 }
 
 Base.extend(Cyborg);
 
-Cyborg.prototype.provider = function(name, fn) {
-  this.providers[name] = fn;
+Cyborg.prototype.addManifest = function(name, manifest) {
+  this.manifests.set(utils.escapeKeys(name), manifest);
+  return this;
 };
 
-Cyborg.prototype.resolve = function(dependency, version, cb) {
-  if (typeof version === 'function') {
-    cb = version;
-    version = null;
-  }
+Cyborg.prototype.getManifest = function(name) {
+  return this.manifests.get(utils.escapeKeys(name));
+};
 
+Cyborg.prototype.install = function(src, options, cb) {
   var self = this;
-  var keys = Object.keys(this.providers);
-  var i = 0;
 
-  next();
-  function next() {
-    var provider = self.providers[keys[i++]];
-    if (!provider) return cb();
-
-    provider(dependency, version, function(err, loader) {
-      if (err) return cb(err);
-      if (loader) return cb(null, loader);
-      next();
-    });
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
   }
-};
 
-Cyborg.prototype.install = function(dependency, version, cb) {
-  if (typeof version === 'function') {
-    cb = version;
-    version = null;
+  if (typeof src === 'object') {
+    return this.installManifest(this.resolveProvider('fs'), src, options, cb);
   }
-  var config = {
-    dest: path.join(process.cwd(), '.cyborg')
+
+  var manifestConfig = {
+    cwd: src,
+    src: this.option('manifestfile') || 'package.json'
   };
 
-  this.resolve(dependency, version, function(err, installer) {
+  var provider = this.resolveProvider(manifestConfig);
+  provider.getManifest(manifestConfig, function(err, manifest) {
     if (err) return cb(err);
-    if (!installer) {
-      return cb(new Error('Unable to resolve a valid installer for ' + dependency + ' ' + (version || '')));
-    }
-    installer.install(dependency, version, function(err, files) {
-      if (err) return cb(err);
-      utils.async.each(files, function(file, next) {
-        utils.writeFile(path.join(config.dest, dependency, file.path), file.content, next);
-      }, cb);
-    });
+    if (utils.isEmpty(manifest)) return cb();
+    self.addManifest(src, manifest);
+    manifest.cwd = src;
+    self.installManifest(provider, manifest, options, cb);
   });
 };
 
-Cyborg.prototype.update = function(dependency, version, cb) {};
-Cyborg.prototype.save = function(dependency, version, cb) {};
+Cyborg.prototype.installFiles = function(provider, config, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+  var expanded = utils.expand(utils.extend({}, config));
+  provider.fetch(expanded, function(err, files) {
+    if (err) return cb(err);
+    console.log(config.cwd, files);
+    console.log();
+    cb();
+  });
+};
+
+Cyborg.prototype.installDependencies = function(dependencies, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+  utils.async.eachOf(dependencies, function(dependency, name, next) {
+    this.install(dependency, options, next);
+  }.bind(this), cb);
+};
+
+Cyborg.prototype.installTargets = function(provider, config, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+  var targets = config.targets || {};
+  utils.async.eachOf(targets, function(target, name, next) {
+    target.cwd = config.cwd;
+    this.installFiles(provider, target, options, next);
+  }.bind(this), cb);
+};
+
+Cyborg.prototype.installManifest = function(provider, manifest, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+  var cwd = manifest.cwd || process.cwd();
+  var workflow = [];
+  var config = {
+    cwd: cwd,
+    dest: '~/.cyborg/' + cwd
+  };
+  if (manifest.files) {
+    config.src = manifest.files
+    workflow.push(utils.async.apply(this.installFiles.bind(this), provider, config, options));
+  }
+
+  if (manifest.targets) {
+    config.targets = manifest.targets;
+    workflow.push(utils.async.apply(this.installTargets.bind(this), provider, config, options));
+  }
+
+  if (manifest.dependencies) {
+    workflow.push(utils.async.apply(this.installDependencies.bind(this), manifest.dependencies, options));
+  }
+
+  utils.async.series(workflow, cb);
+};
+
+Cyborg.prototype.update = function(src, options, cb) {
+  return this.install.apply(this, arguments);
+};
 
 module.exports = Cyborg;
